bdcab8b8c53259754a24641668dd7f7690d414f6
Tania Fabiola <fania@Eduardos-MacBook-Air.local>
2023-07-06 17:24:00 -0600

    proyect

--- src/data.js
+++ src/data.js
@@ -1,9 +1,48 @@
-// estas funciones son de ejemplo
+import ghibli from "./data/ghibli/ghibli.js";
 
-export const example = () => {
-  return 'example';
+export const getData = () => {
+  return ghibli.films;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//criteria can be "ASC" or "DESC"
+export const sortByDate = (listToSort, criteria) => {
+  let sortedFilms = [];
+  if (criteria === "ASC") {
+    sortedFilms = listToSort.sort((a, b) => {
+      if (a.release_date < b.release_date) return -1;
+      if (a.release_date > b.release_date) return 1;
+      return 0;
+    });
+  } else {
+    sortedFilms = listToSort.sort((a, b) => {
+      if (a.release_date > b.release_date) return -1;
+      if (a.release_date < b.release_date) return 1;
+      return 0;
+    });
+  }
+  return sortedFilms;
 };
+
+export const sortByScore = (listToSort, criteria) => {
+  let sortedFilmsScore = [];
+  if (criteria === "byScore") {
+    sortedFilmsScore = listToSort.sort((a, b) => {
+      if (Number(a.rt_score) > Number(b.rt_score)) return -1;
+      if (Number(a.rt_score) < Number(b.rt_score)) return 1;
+      return 0;
+    });
+  }
+  return sortedFilmsScore;
+};
+export const filterDirector = (films, selectedDirector) => {
+  const filteredFilms = [];
+  for (let i = 0; i < films.length; i++) {
+    const film = films[i];
+    if (film.director === selectedDirector) {
+      filteredFilms.push(film);
+    }
+  }
+  return filteredFilms;
+};
+
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,10 +1,57 @@
+import { sortByDate, sortByScore, filterDirector } from "../src/data.js";
+import {
+  shortList,
+  shortListSortedDesc,
+  shortListSortedAsc,
+  shortListSortedScore,
+  shortListFilteredByDirectorIsaoTakahata,
+  shortListFilteredByDirectorHayaoMiyazaki,
+} from "./testdata.js";
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("sortbyDate", () => {
+  it("should be a function", () => {
+    expect(typeof sortByDate).toBe("function");
   });
 
+  it("should return the list sorted by ascending date", () => {
+    expect(sortByDate(shortList, "ASC")).toStrictEqual(shortListSortedAsc);
+  });
 
+  it("should return the list sorted by descending date", () => {
+    expect(sortByDate(shortList, "DESC")).toStrictEqual(shortListSortedDesc);
+  });
+});
+describe("sortByScore", () => {
+  it("should be a function", () => {
+    expect(typeof sortByScore).toBe("function");
+  });
 
+  it("should return the list sorted by descending score", () => {
+    expect(sortByScore(shortList, "byScore")).toStrictEqual(
+      shortListSortedScore
+    );
+  });
+});
+describe("filterDirector", () => {
+  it("should be a function", () => {
+    expect(typeof filterDirector).toBe("function");
+  });
+  it("should return the list filtered by Isao Takahata", () => {
+    expect(filterDirector(shortList, "Isao Takahata")).toStrictEqual(
+      shortListFilteredByDirectorIsaoTakahata
+    );
+  });
+  it("should return the list filtered by Hayao Miyazaki", () => {
+    expect(filterDirector(shortList, "Hayao Miyazaki")).toStrictEqual(
+      shortListFilteredByDirectorHayaoMiyazaki
+    );
+  });
 
+  // it('should return the list filtered by director Hayao Miyazaki', () => {
+  //   const toTestResult = filterDirector(shortList, "Hayao Miyazaki")
+  //   const isEveryDirectorHayao = toTestResult.every((film) => {
+  //     return film.director === "Hayao Miyazaki"
+  //   });
+  //   expect(isEveryDirectorHayao).toBe(true);
+  // });
+});
